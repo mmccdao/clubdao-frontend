55377f5ad46a39e6e3c16fe1664662a47e030888
Rove Mind <roveminder@gmail.com>
2021-11-12 00:11:39 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>ClubDAO</title>
+        <meta name="description" content="A community-owned and operated organization for Meerkats to grow, innovate, have fun, and make impact in the Solana NFT space together" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Join ClubDAO
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
