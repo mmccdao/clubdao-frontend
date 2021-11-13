2d7f2a4b94c17bf5f1bb682f817dda85be611a44
Rove Mind <roveminder@gmail.com>
2021-11-13 12:07:31 +0100

    feat: add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Header from '../components/Header'
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -11,6 +13,8 @@ const Home: NextPage = () => {
         <meta name="description" content="A community-owned and operated organization for Meerkats to grow, innovate, have fun, and make impact in the Solana NFT space together" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
