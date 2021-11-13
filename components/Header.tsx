0f579ed913366b1c988b37d66aef67fb081fad2d
Rove Mind <roveminder@gmail.com>
2021-11-13 13:39:38 +0100

    fix: use Link instead of a

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,184 +6,196 @@ const Header: FC = () => {
 
   return (
     <div className="bg-black sticky top-0 z-50">
-    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div className="relative flex items-center justify-between">
-        <a
-          href="/"
-          aria-label="ClubDAO"
-          title="ClubDAO"
-          className="inline-flex items-center"
-        >
-          {/* TODO Add logo (SVG) */}
-          <span className="ml-2 text-3xl font-bold tracking-wide text-gray-100 uppercase">
-            ClubDAO
-          </span>
-        </a>
-        <ul className="flex items-center hidden space-x-8 lg:flex">
-          <li>
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <Link href="/">
             <a
-              href="/"
-              aria-label="Activities"
-              title="Activities"
-              className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
+              aria-label="ClubDAO"
+              title="ClubDAO"
+              className="inline-flex items-center"
             >
-              Who We Are
+              {/* TODO Add logo (SVG) */}
+              <span className="ml-2 text-3xl font-bold tracking-wide text-gray-100 uppercase">
+                ClubDAO
+              </span>
             </a>
-          </li>
-          <li>
-            <a
-              href="/"
-              aria-label="Activities"
-              title="Activities"
-              className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
-            >
-              Activities
-            </a>
-          </li>
-          <li>
-            <a
-              href="/"
-              aria-label="Roadmap"
-              title="Roadmap"
-              className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
-            >
-              Roadmap
-            </a>
-          </li>
-          <li>
-            <a
-              href="/"
-              aria-label="FAQ"
-              title="FAQ"
-              className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
-            >
-              FAQ
-            </a>
-          </li>
-          <li>
-            <a
-              href="/"
-              aria-label="Team"
-              title="Team"
-              className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
-            >
-              Team
-            </a>
+          </Link>
+          <ul className="flex items-center hidden space-x-8 lg:flex">
+            <li>
+              <Link href="#who-we-are">
+                <a
+                  aria-label="Who We Are"
+                  title="Who We Are"
+                  className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
+                >
+                  Who We Are
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#activities">
+                <a
+                  aria-label="Activities"
+                  title="Activities"
+                  className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
+                >
+                  Activities
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#roadmap">
+                <a
+                  aria-label="Roadmap"
+                  title="Roadmap"
+                  className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
+                >
+                  Roadmap
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#faq">
+                <a
+                  aria-label="FAQ"
+                  title="FAQ"
+                  className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
+                >
+                  FAQ
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#team">
+                <a
+                  aria-label="Team"
+                  title="Team"
+                  className="font-medium text-xl tracking-wide text-gray-100 transition-colors duration-200 hover:text-gray-400"
+                >
+                  Team
+                </a>
+              </Link>
           </li>
         </ul>
-        <div className="lg:hidden">
-          <button
-            aria-label="Open Menu"
-            title="Open Menu"
-            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
-            onClick={() => setIsMenuOpen(true)}
-          >
-            {/* TODO Add logo (SVG) */}
-            <svg className="w-5 text-gray-100" viewBox="0 0 24 24">
-              <path
-                fill="currentColor"
-                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-              />
-              <path
-                fill="currentColor"
-                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-              />
-              <path
-                fill="currentColor"
-                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-              />
-            </svg>
-          </button>
-          {isMenuOpen && (
-            <div className="absolute top-0 left-0 w-full">
-              <div className="p-5 bg-white border rounded shadow-sm">
-                <div className="flex items-center justify-between mb-4">
-                  <div>
-                    <a
-                      href="/"
-                      aria-label="ClubDAO"
-                      title="ClubDAO"
-                      className="inline-flex items-center"
-                    >
-                      {/* TODO Add logo (SVG) */}
-                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                        ClubDAO
-                      </span>
-                    </a>
+          <div className="lg:hidden">
+            <button
+              aria-label="Open Menu"
+              title="Open Menu"
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              {/* TODO Add logo (SVG) */}
+              <svg className="w-5 text-gray-100" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                />
+              </svg>
+            </button>
+            {isMenuOpen && (
+              <div className="absolute top-0 left-0 w-full">
+                <div className="p-5 bg-white border rounded shadow-sm">
+                  <div className="flex items-center justify-between mb-4">
+                    <div>
+                      <Link href="/">
+                        <a
+                          aria-label="ClubDAO"
+                          title="ClubDAO"
+                          className="inline-flex items-center"
+                        >
+                          {/* TODO Add logo (SVG) */}
+                          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                            ClubDAO
+                          </span>
+                        </a>
+                      </Link>
+                    </div>
+                    <div>
+                      <button
+                        aria-label="Close Menu"
+                        title="Close Menu"
+                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                          <path
+                            fill="currentColor"
+                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
-                  <div>
-                    <button
-                      aria-label="Close Menu"
-                      title="Close Menu"
-                      className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                      onClick={() => setIsMenuOpen(false)}
-                    >
-                      <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                        <path
-                          fill="currentColor"
-                          d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                        />
-                      </svg>
-                    </button>
-                  </div>
+                  <nav>
+                    <ul className="space-y-4">
+                      <li>
+                        <Link href="#who-we-are">
+                          <a
+                            aria-label="Who We Are"
+                            title="Who We Are"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Who We Are
+                          </a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="#activities">
+                          <a
+                            aria-label="Activities"
+                            title="Activities"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Activities
+                          </a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="#roadmap">
+                          <a
+                            aria-label="Roadmap"
+                            title="Roadmap"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Roadmap
+                          </a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="#faq">
+                          <a
+                            aria-label="FAQ"
+                            title="FAQ"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            FAQ
+                          </a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="#team">
+                          <a
+                            aria-label="Team"
+                            title="Team"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Team
+                          </a>
+                        </Link>
+                    </li>
+                    </ul>
+                  </nav>
                 </div>
-                <nav>
-                  <ul className="space-y-4">
-                    <li>
-                      <a
-                        href="#who-we-are"
-                        aria-label="Who We Are"
-                        title="Who We Are"
-                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >
-                        Who We Are
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#activities"
-                        aria-label="Activities"
-                        title="Activities"
-                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >
-                        Activities
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#roadmap"
-                        aria-label="Roadmap"
-                        title="Roadmap"
-                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >
-                        Roadmap
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#faq"
-                        aria-label="FAQ"
-                        title="FAQ"
-                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >
-                        FAQ
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#team"
-                        aria-label="Team"
-                        title="Team"
-                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >
-                        Team
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
               </div>
-            </div>
-          )}
+            )}
           </div>
         </div>
       </div>
