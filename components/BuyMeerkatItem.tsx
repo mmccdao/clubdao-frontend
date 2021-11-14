7719665cea372b3871dd181f4102c53c9834e7be
Rove Mind <roveminder@gmail.com>
2021-11-14 15:24:11 +0100

    feat: add focus and hover on BuyMeerkatItem to show Meerkat number

--- components/BuyMeerkatItem.tsx
+++ components/BuyMeerkatItem.tsx
@@ -17,7 +17,7 @@ const BuyMeerkatItem: FC<Props> = ({key, meerkat, market}) => {
       <Link href={meerkat.market.url}>
         <a
           target="_blank"
-          className="block rounded-md border-8 border-gray-100 p-0 w-72 h-72"
+          className="relative block rounded-md border-8 border-gray-100 p-0 w-72 h-72"
         >
           <Image
             src={meerkat.image}
@@ -26,10 +26,13 @@ const BuyMeerkatItem: FC<Props> = ({key, meerkat, market}) => {
             width="300px"
             height="300px"
           />
+          <div className="absolute flex top-0 h-full w-full opacity-0 focus:opacity-100 hover:opacity-100 p-2 justify-center items-end">
+            <p className="relative rounded-md mb-2 p-2 px-4 font-semibold text-xl text-gray-900 bg-gray-100">{meerkat.name}</p>
+          </div>
         </a>
       </Link>
       <Link href={market.url}>
-        <a className="block w-44 -mt-2 py-1 rounded-md bg-black text-center border-8">
+        <a className="block w-44 -mt-2 py-1 rounded-md bg-black focus:bg-pink-700 hover:bg-pink-700 text-center border-8">
           Buy on
           <br /> {market.name}
         </a>
