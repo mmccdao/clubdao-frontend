bfc851b76267f57416d2c895313a838756eab74c
Rove Mind <roveminder@gmail.com>
2021-11-17 16:56:08 +0100

    fix: improve activities content sizes

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -22,18 +22,18 @@ const Activity: FC = () => {
                 src="/marketing-collaboration.png"
                 alt="Marketing Collaborations"
                 layout="fixed"
-                width="180px"
-                height="180px"
+                width="96"
+                height="96"
               />
             </div>
-            <p className="text-3xl font-semibold lg:text-5xl">
+            <p className="text-2xl font-semibold lg:text-3xl">
               Marketing Collaborations
             </p>
-            <p className="py-6 text-2xl">
+            <p className="py-6 text-lg lg:text-xl">
               We partner with other new and established NFT projects for
               co-promotion and other collaborations.
             </p>
-            <p className="text-2xl">
+            <p className="text-lg lg:text-xl">
               <Link href="https://twitter.com/mmcclubdao">
                 <a
                   className="underline hover:no-underline focus:text-pink-700"
@@ -51,18 +51,20 @@ const Activity: FC = () => {
                 src="/education.png"
                 alt="Crypto Education"
                 layout="fixed"
-                width="180px"
-                height="180px"
+                width="96"
+                height="96"
               />
             </div>
-            <p className="text-3xl font-semibold lg:text-5xl">
-              Crypto Education
+            <p className="text-2xl font-semibold lg:text-3xl">
+              Crypto
+              <br />
+              Education
             </p>
-            <p className="py-6 text-2xl">
+            <p className="py-6 text-lg lg:text-xl">
               We create programs and media to drive awareness, skill building,
               and safety in the crypto and Solana NFT spaces.
             </p>
-            <p className="text-2xl">Details TBA.</p>
+            <p className="text-lg lg:text-xl">Details TBA.</p>
           </div>
           <div className="flex flex-col w-80 items-center text-center">
             <div className="mt-6 lg:mt-0 mb-6">
@@ -70,19 +72,20 @@ const Activity: FC = () => {
                 src="/beer-cheers.png"
                 alt="Events"
                 layout="fixed"
-                width="180px"
-                height="180px"
+                width="96"
+                height="96"
               />
             </div>
-            <p className="text-3xl font-semibold lg:text-5xl">
+            <p className="text-2xl font-semibold lg:text-3xl">
               Community
+              <br />
               Events
             </p>
-            <p className="py-6 text-2xl">
+            <p className="py-6 text-lg lg:text-xl">
               We host local and virtual events for members and guests to meet,
               socialize, share alpha, and build long-lasting relationships.
             </p>
-            <p className="text-2xl">
+            <p className="text-lg lg:text-xl">
               <Link href="https://twitter.com/mmcclubdao">
                 <a
                   className="underline hover:no-underline focus:text-pink-700"
