bc27b7fbc21896af76e3d0e152c7a8ba58fb33c0
Rove Mind <roveminder@gmail.com>
2021-11-17 12:59:41 +0100

    feat: responsive tile

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -11,12 +11,12 @@ interface Props {
 const Tile = (props: Props) => {
   const { color, description, icon, incomplete, title } = props;
   return (
-    <div className={"min-h-64 color-gray-200 border-4 p-4 rounded-md border-gray-200 " + (incomplete ? 'border-dashed': '')}>
-      <h4 className="flex mb-2 items-center text-md lg:text-lg xl:text-xl font-semibold">
-        <span className={`rounded-full mr-4 inline h-14 w-14 lg:h-18 lg:w-18 flex flex-shrink-0 items-center justify-center ${color}`}>{icon}</span>
+    <div className={"min-h-64 color-gray-200 border-2 p-4 rounded-md border-gray-200 " + (incomplete ? 'border-dashed': '')}>
+      <h4 className="flex mb-2 items-center text-xl lg:text-2xl font-semibold">
+        <span className={`rounded-full mr-4 inline h-16 w-16 p-3 lg:h-18 lg:w-18 flex flex-shrink-0 items-center justify-center ${color}`}>{icon}</span>
         {title}
       </h4>
-      <p className="font-extralight">{description}</p>
+      <p className="p-2 font-extralight text-lg lg:text-xl">{description}</p>
     </div>
   );
 }
